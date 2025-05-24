9ef55d930545f4432dc2addc4dbddbe8a53906be
josephthomas0313 <josephthomas0313@gmail.com>
2025-05-24 21:58:42 +0000

    updated with the notification of "contact details coming soon"

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,17 +2,19 @@ export default function Contact() {
     return (
       <div className="p-6 text-center text-green-800">
         <h2 className="text-3xl font-bold mb-4">📞 Contact Information</h2>
+        <h3>This is the contact section. Details will be added here soon.</h3>
+        <br></br>
+        <br></br>
         <iframe
         src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d6135.263456979545!2d76.8848271!3d9.6704605!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3b07b5fc7a9f4611%3A0x4cbe85a01027f40b!2sVagamon%20Tent%20campingn!5e1!3m2!1sen!2sin!4v1747517544168!5m2!1sen!2sin"
-        width="600"
-        height="450"
+        width="1300"
+        height="350"
         style={{ border: 0 }}
         allowFullScreen=""
         loading="lazy"
         referrerPolicy="no-referrer-when-downgrade"
       >
       </iframe>
-      <p>This is the contact section. Details will be added here soon.</p>
       </div>
     );
   }
